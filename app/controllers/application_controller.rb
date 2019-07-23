fea0c468bc16217ff0976e8bb7a31c1533b51645
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-07-23 11:35:12 -0300

    solved non-branch-conflicting rubocop offenses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-        include DeviseTokenAuth::Concerns::SetUserByToken
-  protect_from_forgery with: :null_session, only: Proc.new { |c| c.request.format.json? }
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  protect_from_forgery with: :null_session,
+                       only: proc { |c| c.request.format.json? }
 end
