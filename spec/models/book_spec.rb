3076fee2dab66aac9bccc085d3d858048829fd5d
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-07-24 13:40:59 -0300

    changed should method for is_expected.to

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -6,10 +6,10 @@ describe Book do
   subject(:book) { FactoryBot.create(:book) }
 
   it { is_expected.to be_valid }
-  it { should validate_presence_of(:genre) }
-  it { should validate_presence_of(:author) }
-  it { should validate_presence_of(:image) }
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:editor) }
-  it { should validate_presence_of(:year) }
+  it { is_expected.to validate_presence_of(:genre) }
+  it { is_expected.to validate_presence_of(:author) }
+  it { is_expected.to validate_presence_of(:image) }
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:editor) }
+  it { is_expected.to validate_presence_of(:year) }
 end
