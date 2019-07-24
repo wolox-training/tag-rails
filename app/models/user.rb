579039b391cf1c51c1ebd17b5cf30d8d6faca578
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-07-24 13:22:35 -0300

    added password confirmation validation + email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, :email, :password,
             presence: true
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, confirmation: true
 end
