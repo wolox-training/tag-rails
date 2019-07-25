9c7348f8944b4fabdef2ad6d413b1314149ec8b9
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-07-25 14:25:24 -0300

    Changed FactoryBot create method for build in tests + unhappy path tests

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,13 +3,32 @@
 require 'rails_helper'
 
 describe Book do
-  subject(:book) { FactoryBot.create(:book) }
 
-  it { is_expected.to be_valid }
-  it { is_expected.to validate_presence_of(:genre) }
-  it { is_expected.to validate_presence_of(:author) }
-  it { is_expected.to validate_presence_of(:image) }
-  it { is_expected.to validate_presence_of(:title) }
-  it { is_expected.to validate_presence_of(:editor) }
-  it { is_expected.to validate_presence_of(:year) }
+  required_fields = Book.validators.first.attributes
+
+  context 'with correctly typed required fields' do
+    subject { FactoryBot.build(:book) }
+
+    it { is_expected.to be_valid }
+
+    required_fields.each do |field|
+      it { is_expected.to validate_presence_of(field) }
+    end
+
+    it 'should save correctly' do
+      expect(subject.save).to be true
+    end
+  end
+
+  required_fields.each do |field|
+    context "with missing #{field}" do
+      subject { FactoryBot.build(:book, field => nil) }
+
+      it { is_expected.to_not be_valid }
+
+      it 'should not be save' do
+        expect(subject.save).to_not be true
+      end
+    end
+  end
 end
