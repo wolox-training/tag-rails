e32a3bbea75a6cb9d0042a6c649e11f70d416fad
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-07-26 18:33:15 -0300

    fixed rubocop offense

--- app/serializers/books_serializer.rb
+++ app/serializers/books_serializer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class BooksSerializer < ActiveModel::Serializer
   attributes :id, :author, :title, :image, :publisher, :year, :genre,
              :created_at, :updated_at
