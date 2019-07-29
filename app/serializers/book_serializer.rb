8fb7c20e5e599ef30865681e7e0f6eb59761ce3e
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-07-29 15:35:43 -0300

    removed unnecesary definitions in serializers + fixed sql injection

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,16 +1,13 @@
 # frozen_string_literal: true
 
 class BookSerializer < ActiveModel::Serializer
-  attributes :id, :author, :title, :genre, :editor, :year, :image_url
-
+  attributes :id, :author, :title, :genre
+  attribute :editor, key: :publisher
+  attribute :year
+  attribute :image, key: :image_url
   has_one :actual_rent
 
   def actual_rent
-    now = Date.current
-    object.rents.find_by("rent_start <= '#{now}' and '#{now}' <= rent_end")
-  end
-
-  def image_url
-    object.image
+    object.rents.find_by('rent_start <= now() and returned_at is null')
   end
 end
--- app/serializers/rent_serializer.rb
+++ app/serializers/rent_serializer.rb
@@ -1,15 +1,9 @@
 # frozen_string_literal: true
 
 class RentSerializer < ActiveModel::Serializer
-  attributes :id, :from, :to, :book_id, :user_id, :returned_at
-
-  def from
-    object.rent_start
-  end
-
-  def to
-    object.rent_end
-  end
-
-  def returned_at; end
+  attributes :id
+  attribute :rent_start, key: :from
+  attribute :rent_end, key: :to
+  attributes :book_id, :user_id
+  attribute :returned_at
 end
