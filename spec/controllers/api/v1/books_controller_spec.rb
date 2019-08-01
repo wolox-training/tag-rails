b216c18fa04b9db958bff9e80e9a5935c8a84c68
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-08-01 17:09:42 -0300

    changed tests to use to eq + unhappy paths

--- spec/controllers/api/v1/books_controller_spec.rb
+++ spec/controllers/api/v1/books_controller_spec.rb
@@ -3,21 +3,23 @@
 require 'rails_helper'
 
 describe Api::V1::BooksController, type: :controller do
+  include Wor::Paginate
   include_context 'with Authenticated User'
 
   describe 'GET #index' do
     context 'when fetching all books' do
-      let!(:books) { create_list(:book, 3) }
-
       before do
+        create_list(:book, 3)
         get :index
       end
 
-      it 'responds with a paginated all books json' do
-        expected = ActiveModel::Serializer::CollectionSerializer.new(
-          books, each_serializer: BooksSerializer
-        ).to_json
-        expect(response.body.to_json) =~ JSON.parse(expected)
+      it 'responds with a paginated json' do
+        expect(JSON.parse(response.body)).to be_paginated
+      end
+
+      it 'responds with books that include expected attributes' do
+        book = JSON.parse(response.body, symbolize_names: true)[:page].first
+        expect(book.to_json).to eq BooksSerializer.new(Book.find(book[:id])).to_json
       end
 
       it 'responds with 200 status' do
@@ -27,7 +29,7 @@ describe Api::V1::BooksController, type: :controller do
   end
 
   describe 'GET #show' do
-    context 'when fetching a book' do
+    context 'when fetching a book with correct id' do
       let!(:book) { create(:book) }
 
       before do
@@ -35,13 +37,23 @@ describe Api::V1::BooksController, type: :controller do
       end
 
       it 'responds with the book json' do
-        expect(response.body.to_json) =~ BookSerializer.new(
+        expect(JSON.parse(response.body).to_json).to eq BookSerializer.new(
           book
         ).to_json
       end
 
       it 'responds with 200 status' do
         expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when fetching a book with incorrect id' do
+      before do
+        get :show, params: { id: 1 }
+      end
+
+      it 'returns with not found (404) status' do
+        expect(response).to have_http_status(:not_found)
       end
     end
   end
