1be6166dbf38de6835fb623cfd495d35391cee7f
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-08-01 12:03:12 -0300

    Removed rent serializer + actual_rent moved to Book model
    The rent serializer was removed and a RentSerializer was included in BookSerializer

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -7,7 +7,11 @@ class BookSerializer < ActiveModel::Serializer
   attribute :image, key: :image_url
   has_one :actual_rent
 
-  def actual_rent
-    object.rents.find_by('rent_start <= now() and returned_at is null')
+  class RentSerializer < ActiveModel::Serializer
+    attributes :id
+    attribute :rent_start, key: :from
+    attribute :rent_end, key: :to
+    attributes :book_id, :user_id
+    attribute :returned_at
   end
 end
