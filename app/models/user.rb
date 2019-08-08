54b119b24a8338aaf1de15d51df24cbc93e77543
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-08-08 10:08:07 -0300

    Presence validator for locale added to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, :locale, presence: true
 end
