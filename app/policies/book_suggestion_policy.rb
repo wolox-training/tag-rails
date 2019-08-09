5b4df6d5c39cfb8bbd9533f74b974cbb41e2dd2c
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-08-09 16:34:13 -0300

    Removed unnecessary scope class in BookSuggestionPolicy

--- app/policies/book_suggestion_policy.rb
+++ app/policies/book_suggestion_policy.rb
@@ -2,10 +2,4 @@ class BookSuggestionPolicy < ApplicationPolicy
   def permitted_attributes_for_create
     %i[synopsis price author title link editor year user_id]
   end
-
-  class Scope < Scope
-    def resolve
-      scope.all
-    end
-  end
 end
