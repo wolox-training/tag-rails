3e463ac393aabafcc8de5aa6934f7c26e445553d
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-08-12 13:36:28 -0300

    Added migration to limit

---
+++ db/migrate/20190812154733_limit_book_title_length.rb
@@ -0,0 +1,14 @@
+class LimitBookTitleLength < ActiveRecord::Migration[5.1]
+  def up
+    Book.find_each do |book|
+      book.title = book.title.truncate 25
+      book.save!
+    end
+
+    change_column :books, :title, :string, limit: 25
+  end
+
+  def down
+    change_column :books, :title, :string, limit: 255
+  end
+end
