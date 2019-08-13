50f2d63c5c5fdc3bfa3418ab949045be2c8edc07
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-08-13 17:46:09 -0300

    Added migration to limit amount of characters in Book title field (#22)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190806194203) do
+ActiveRecord::Schema.define(version: 20190812154733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20190806194203) do
     t.string "genre", null: false
     t.string "author", null: false
     t.string "image", null: false
-    t.string "title", null: false
+    t.string "title", limit: 25, null: false
     t.string "editor", null: false
     t.string "year", null: false
     t.datetime "created_at", null: false
