d8e164c0a62e3fdf019704ab57012e8065e5e5bc
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-08-20 14:13:53 -0300

    typo fixes and removed unnecesary script lines

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /app/tmp/pids/server.pid
+rm -f /myapp/tmp/pids/server.pid
+
+rake db:create db:migrate
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
